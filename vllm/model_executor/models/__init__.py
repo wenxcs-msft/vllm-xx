25b5b0517b3b2f77246c18f7bb624562b00ccbac
Zeqi Lin <Zeqi.Lin@microsoft.com>
2024-08-08 13:54:30 +0800

    mixtral

--- vllm/model_executor/models/__init__.py
+++ vllm/model_executor/models/__init__.py
@@ -47,7 +47,7 @@ _GENERATION_MODELS = {
     # For decapoda-research/llama-*
     "LLaMAForCausalLM": ("llama", "LlamaForCausalLM"),
     "MistralForCausalLM": ("llama", "LlamaForCausalLM"),
-    "MixtralForCausalLM": ("mixtral", "MixtralForCausalLM"),
+    "MixtralForCausalLM": ("phimoe", "PhiMoEForCausalLM"),
     "QuantMixtralForCausalLM": ("mixtral_quant", "MixtralForCausalLM"),
     # transformers's mpt class has lower case
     "MptForCausalLM": ("mpt", "MPTForCausalLM"),
